3988aa4b01c0aa385955f7ab94f60a0dbaa5ccdc
Ali Reza Ibrahimzada <alibrahimzada@gmail.com>
2025-07-08 17:24:07 -0400

    Add JavaScript language support

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='staticfg',
       author='Aurelien Coet',
       author_email='aurelien.coet19@gmail.com',
       description='Control flow graph generator for Python3 programs',
-      packages=['python', 'java', 'rust', 'go', 'c'],
+      packages=['python', 'java', 'javascript', 'rust', 'go', 'c'],
       test_suite='tests',
       install_requires=[
         'astor',
